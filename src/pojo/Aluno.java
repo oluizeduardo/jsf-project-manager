6c9a0ca3841e9e56ed2c89ec1ab767755b9e8c02
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2017-08-04 20:45:28 -0300

    First commit JSF frontend

---
+++ src/pojo/Aluno.java
@@ -0,0 +1,42 @@
+package pojo;
+
+
+public class Aluno {
+
+	private String matricula;
+	private String nome;
+	private String curso;
+	private String dataNascimento;
+	
+	
+	public Aluno(){ }
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+}
