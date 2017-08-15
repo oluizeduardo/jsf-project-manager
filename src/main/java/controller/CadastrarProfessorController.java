7360307c0fea2b5cb4818df8398e2c7164ac2c85
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2017-08-15 01:40:42 -0300

    Organizadas as telas para cadastro de alunos e professores

---
+++ src/main/java/controller/CadastrarProfessorController.java
@@ -0,0 +1,73 @@
+package controller;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.event.FlowEvent;
+import model.Professor;
+import model.helperView.ListaDeEstadoCivil;
+import model.helperView.ListaDeEstados;
+import model.helperView.ListaDeIdiomas;
+
+
+@ManagedBean
+@ViewScoped
+public class CadastrarProfessorController {
+
+	
+	private Professor professor = null;
+	private List<String> estadosBrasileiros = null;
+	private List<String> estadoCivil = null;
+	private List<String> idiomas = null;
+	
+	
+	public CadastrarProfessorController() {
+		this.professor = new Professor();
+		this.estadosBrasileiros = new ListaDeEstados().getList();
+		this.estadoCivil = new ListaDeEstadoCivil().getList();
+		this.idiomas = new ListaDeIdiomas().getList();
+	}
+	
+	
+	
+	public String onFlowProcess(FlowEvent event) {
+
+		return event.getNewStep();
+	}
+
+
+	
+	public void salvar(){
+		
+		System.out.println("Dados do professor salvo com sucesso!");
+		
+	}
+	
+	
+	
+	
+	public Professor getProfessor() {
+		return professor;
+	}
+
+
+	public void setAluno(Professor prof) {
+		this.professor = prof;
+	}
+
+	
+	public List<String> getEstadosBrasileiros() {
+		return estadosBrasileiros;
+	}
+	
+	public List<String> getEstadoCivil() {
+		return estadoCivil;
+	}
+	
+	public List<String> getIdiomas() {
+		return idiomas;
+	}
+	
+	
+}
