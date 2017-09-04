ad8d814f6f024c7ed52639eaac15ead4d7a68382
fabiano <fabiano.lomonaco.junior@gmail.com>
2017-09-04 20:20:45 -0300

    Refatoração do código das classes de Login, para verificar um usuário existente e permitir seu acesso

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,55 +1,44 @@
 package controller;
 
 import java.io.IOException;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.swing.JOptionPane;
-
 import model.dao.LoginDAO;
 import model.pojo.Usuario;
-
 
 @ManagedBean(name = "loginController")
 @ViewScoped
 public class LoginController {
 
-	/**
-	 * Objeto do usuário que acessa o sistema.
-	 */
 	private Usuario usuario;
 	private LoginDAO loginDAO = new LoginDAO();
-	
+
 	public LoginController() {
 		this.usuario = new Usuario();
 	}
-	
-	public void acessar(){
-		
+
+	public void acessar() {
+
 		String user = this.usuario.getContato().getEmail();
 		String password = this.usuario.getSenha();
-		
-		 Usuario existeUsuario = loginDAO.verificarLogin(user, password);
-		
-		
-		//System.out.println("Acessando sistema... USUÁRIO: '"+user+"' SENHA: '"+password+"'");
-		
-//		if(existeUsuario) {
-//			System.out.println("AMEM DEUS");
-//			
-////			try {
-////				FacesContext.getCurrentInstance().getExternalContext().redirect("aluno/home.xhtml");
-////			} catch (IOException e) { 
-////				System.err.println(e.getMessage());
-////			}
-//		}
-//		else {
-//			System.out.println("FUDEU");
-//		}
+
+		boolean existeUsuario = loginDAO.verificarLogin(user, password);
+
+		if (existeUsuario) {
+			System.out.println("Logado com sucesso!");
+
+			try {
+				FacesContext.getCurrentInstance().getExternalContext().redirect("aluno/home.xhtml");
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		} else {
+			System.out.println("Não logou!");
+		}
 	}
-	
-	public Usuario getUsuario(){
+
+	public Usuario getUsuario() {
 		return usuario;
 	}
 }
--- src/main/java/model/dao/LoginDAO.java
+++ src/main/java/model/dao/LoginDAO.java
@@ -3,56 +3,44 @@ package model.dao;
 import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
-import org.neo4j.driver.v1.Transaction;
-import org.neo4j.driver.v1.exceptions.ClientException;
-
 import model.dao.databaseconfig.ConnectionNeo4j;
-import model.pojo.Usuario;
 
 public class LoginDAO {
-	
-private Session session = null;
-	
+
+	private Session session = null;
+
 	public LoginDAO() {
 		ConnectionNeo4j neo4j = new ConnectionNeo4j();
 		this.session = neo4j.getSession();
 	}
-	
-	public Usuario verificarLogin(String user, String password) {
-		
-		Usuario usuarioNovo = new Usuario();
-		
+
+	public boolean verificarLogin(String user, String password) {
+
 		System.out.println("VARIAVEIS QUE VIERAM DO PARAMETRO");
 		System.out.println(user);
 		System.out.println(password);
-		
-		//boolean status = true;
-		
-		System.out.println("buscando!!");
-				
-		StatementResult resultado = session.run("MATCH (n) WHERE n.email= '" 
-		+ user + "'and n.senha= '" + password + "' RETURN n.email as email, n.senha as senha");
-		
-		while(resultado.hasNext()) {
+		System.out.println("realizando busca!!");
+
+		StatementResult resultado = session.run("MATCH (n) WHERE n.email= '" + user + "'and n.senha= '" + password
+				+ "' RETURN n.email as email, n.senha as senha");
+
+		String emailAux = "";
+		String senhaAux = "";
+
+		while (resultado.hasNext()) {
 			Record registro = resultado.next();
-			String email = registro.get("email").asString();
-			String senha = registro.get("senha").asString();
-			
-			Usuario usuarioAux = new Usuario();
-			usuarioAux.getContato().setEmail(email);
-			usuarioAux.setSenha(senha);
-			
-			System.out.println("VARIAVEIS DEPOIS DA BUSCA");
-			System.out.println(usuarioAux.getSenha());
-			System.out.println(usuarioAux.getContato().getEmail());
-			
-			usuarioNovo = usuarioAux;
+			emailAux = registro.get("email").asString();
+			senhaAux = registro.get("senha").asString();
 		}
-		
+
 		session.close();
-		return usuarioNovo;
+
+		if (emailAux.equals(user) && senhaAux.equals(password)) {
+			return true;
+		}
+
+		else {
+			return false;
+		}
 	}
-		
 }
-
-
