928b4b6b8c3b7bc5d076b6ef26390091de949bd3
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2017-09-09 11:27:46 -0300

    Implementação da´página de relatórios do professor

--- src/main/java/controller/RelatoriosController.java
+++ src/main/java/controller/RelatoriosController.java
@@ -1,7 +1,7 @@
 package controller;
 
 import javax.faces.bean.ManagedBean;
-
+import javax.faces.bean.ViewScoped;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
@@ -9,11 +9,14 @@ import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.HorizontalBarChartModel;
 
 @ManagedBean
+@ViewScoped
 public class RelatoriosController {
 
+	
 	private BarChartModel barModel;
     private HorizontalBarChartModel horizontalBarModel;
  
+    
     public RelatoriosController() {
         createBarModels();
     }
@@ -26,85 +29,28 @@ public class RelatoriosController {
         return horizontalBarModel;
     }
  
+    
     private BarChartModel initBarModel() {
         BarChartModel model = new BarChartModel();
  
         ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 120);
-        boys.set("2005", 100);
-        boys.set("2006", 44);
-        boys.set("2007", 150);
-        boys.set("2008", 25);
- 
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 110);
-        girls.set("2007", 135);
-        girls.set("2008", 120);
+        boys.setLabel("Sistemas de Informação");
+        boys.set("Java", 120);
+        boys.set("Banco de Dados", 150);
+        boys.set("Python", 25);
  
         model.addSeries(boys);
-        model.addSeries(girls);
          
         return model;
     }
      
     private void createBarModels() {
-        createBarModel();
-        createHorizontalBarModel();
+        createBarModel();  
     }
      
     private void createBarModel() {
-        barModel = initBarModel();
-         
-        barModel.setTitle("Bar Chart");
+        barModel = initBarModel();         
         barModel.setLegendPosition("ne");
-         
-        Axis xAxis = barModel.getAxis(AxisType.X);
-        xAxis.setLabel("Gender");
-         
-        Axis yAxis = barModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Births");
-        yAxis.setMin(0);
-        yAxis.setMax(200);
-    }
-     
-    private void createHorizontalBarModel() {
-        horizontalBarModel = new HorizontalBarChartModel();
- 
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 50);
-        boys.set("2005", 96);
-        boys.set("2006", 44);
-        boys.set("2007", 55);
-        boys.set("2008", 25);
- 
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 82);
-        girls.set("2007", 35);
-        girls.set("2008", 120);
- 
-        horizontalBarModel.addSeries(boys);
-        horizontalBarModel.addSeries(girls);
-         
-        horizontalBarModel.setTitle("Horizontal and Stacked");
-        horizontalBarModel.setLegendPosition("e");
-        horizontalBarModel.setStacked(true);
-         
-        Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
-        xAxis.setLabel("Births");
-        xAxis.setMin(0);
-        xAxis.setMax(200);
-         
-        Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Gender");        
-    }
- 
+    } 
     
 }
