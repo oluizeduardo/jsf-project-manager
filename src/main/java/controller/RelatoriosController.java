8f1ebe745c297d31900b9a69f5c9fe1a465ed381
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2017-10-08 12:36:00 -0300

    Implementação dos gráficos na página de relatórios.

--- src/main/java/controller/RelatoriosController.java
+++ src/main/java/controller/RelatoriosController.java
@@ -2,6 +2,9 @@ package controller;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.HorizontalBarChartModel;
@@ -10,45 +13,67 @@ import org.primefaces.model.chart.HorizontalBarChartModel;
 @ViewScoped
 public class RelatoriosController {
 
-	
+	// Modelo do gráfico.
 	private BarChartModel barModel;
-    private HorizontalBarChartModel horizontalBarModel;
+	private HorizontalBarChartModel horizontalBarModel;
+    
+	
+    public RelatoriosController() {
+        createBarModel();
+        createHorizontalBarModel();
+    }
  
     
-    public RelatoriosController() {
-        createBarModels();
-    }
- 
-    public BarChartModel getBarModel() {
-        return barModel;
-    }
-     
     public HorizontalBarChartModel getHorizontalBarModel() {
         return horizontalBarModel;
+    }
+    public BarChartModel getBarModel() {
+        return barModel;
     }
  
     
     private BarChartModel initBarModel() {
         BarChartModel model = new BarChartModel();
  
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Sistemas de Informação");
-        boys.set("Java", 60);
-        boys.set("Banco de Dados", 45);
-        boys.set("Android", 10);
+        ChartSeries chart = new ChartSeries();
+        chart.setLabel("Sistemas de Informação");
+        chart.set("Java", 60);
+        chart.set("Banco de Dados", 45);
+        chart.set("Android", 10);
  
-        model.addSeries(boys);
+        model.addSeries(chart);
          
         return model;
     }
      
-    private void createBarModels() {
-        createBarModel();  
-    }
      
     private void createBarModel() {
         barModel = initBarModel();         
         barModel.setLegendPosition("ne");
     } 
     
+    
+    private void createHorizontalBarModel() {
+        horizontalBarModel = new HorizontalBarChartModel();
+ 
+        ChartSeries serie = new ChartSeries();
+        serie.setLabel("Professores");
+        serie.set("Thais Almeida", 10);
+        serie.set("Roberto Dias", 15);
+        serie.set("Rony Gomes", 22);
+        serie.set("André Oliveira", 30);
+        serie.set("João Paulo Silva", 50);
+        
+ 
+        horizontalBarModel.addSeries(serie);
+         
+        horizontalBarModel.setTitle("Número de Projetos Cadastrados pelos Professores");
+        horizontalBarModel.setLegendPosition("e");
+        horizontalBarModel.setStacked(true);
+         
+        Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
+        xAxis.setMin(new Integer(0));
+        xAxis.setMax(new Integer(60));
+    }
+
 }
