7d7ef579dfc686fb2d5f651d2ed661a073fb615a
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2017-10-28 19:03:33 -0200

    Adicionado gráfico de linhas na página de relatórios.

--- src/main/java/controller/RelatoriosController.java
+++ src/main/java/controller/RelatoriosController.java
@@ -7,8 +7,11 @@ import javax.faces.bean.ViewScoped;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.HorizontalBarChartModel;
+import org.primefaces.model.chart.LineChartModel;
+
 import model.dao.RelatorioDAO;
 import model.pojo.Pessoa;
 import web.SessionUtil;
@@ -21,6 +24,8 @@ public class RelatoriosController {
 	private BarChartModel barModel;
 	// Modelo do gráfico horizontal.
 	private HorizontalBarChartModel horizontalBarModel;
+	// Modelo de gráfico de linhas.
+	private LineChartModel lineModel;
 	// Objeto de acesso aos dados.
 	private RelatorioDAO relatorioDAO = new RelatorioDAO();
     // Curso selecionado para consultar as habilidades mais comuns.
@@ -34,6 +39,7 @@ public class RelatoriosController {
     	
         createVerticalBarModel();
         createHorizontalBarModel();
+        createLineModel();
     }
 
     
@@ -66,7 +72,7 @@ public class RelatoriosController {
         	chart.set("Nenhuma habilidade localizada para "+cursoSelecionado, 0);
         }else{
         	// Coloca até 5 habilidades no gráfico.
-        	for (int i=0; i < lista.size() && i < 5; i++) {
+        	for (int i=0; i < lista.size() && i < 3; i++) {
         		Map<String, Object> map = lista.get( i );
     			String habilidade = (String) map.get("Habilidade");
     			Integer qtdeProjetos = (Integer) map.get("Qtde");
@@ -109,7 +115,7 @@ public class RelatoriosController {
         if(lista.isEmpty()){
         	serie.set("Ninguém cadastrou ainda", 0);
         }else{
-        	for (int i=0; i < lista.size() && i < 10; i++) {
+        	for (int i=0; i < lista.size() && i < 8; i++) {
         		Map<String, Object> map = lista.get( i );
     			String nome = (String) map.get("Nome");
     			Integer qtdeProjetos = (Integer) map.get("Qtde");
@@ -121,7 +127,7 @@ public class RelatoriosController {
         }        
 
         horizontalBarModel.addSeries(serie);         
-        horizontalBarModel.setTitle("Número de Projetos Cadastrados");
+        horizontalBarModel.setTitle("Número de Projetos Por Professor");
         horizontalBarModel.setStacked(true);
         horizontalBarModel.setAnimate(true);
         horizontalBarModel.setShadow(false);
@@ -132,6 +138,62 @@ public class RelatoriosController {
         xAxis.setMax(new Integer(maiorQtde+2));
         xAxis.setTickCount(3);
     }
+    
+    
+    
+    /**
+     * Cria um gráfico de linhas.
+     */
+    private void createLineModel(){
+    	
+    	lineModel = new LineChartModel();
+    	lineModel.setTitle("Projetos Cadastrados Durante o Ano");
+        lineModel.setLegendPosition("e");
+        lineModel.setShowPointLabels(true);
+        lineModel.getAxes().put(AxisType.X, new CategoryAxis("Meses"));
+        lineModel.setAnimate(true);
+        
+        ChartSeries serie1 = new ChartSeries();
+        serie1.setLabel("Sistemas de Informação");
+        serie1.set("Jan", 2);
+        serie1.set("Fev", 3);
+        serie1.set("Mar", 4);
+        serie1.set("Abr", 1);
+        serie1.set("Mai", 2);
+        serie1.set("Jun", 2);
+        serie1.set("Jul", 1);
+        serie1.set("Ago", 1);
+        serie1.set("Set", 4);
+        serie1.set("Out", 1);
+        serie1.set("Nov", 2);
+        serie1.set("Dez", 2);
+ 
+        ChartSeries serie2 = new ChartSeries();
+        serie2.setLabel("Administração");
+        serie2.set("Jan", 1);
+        serie2.set("Fev", 1);
+        serie2.set("Mar", 6);
+        serie2.set("Abr", 7);
+        serie2.set("Mai", 2);
+        serie2.set("Jun", 0);
+        serie2.set("Jul", 0);
+        serie2.set("Ago", 3);
+        serie2.set("Set", 3);
+        serie2.set("Out", 0);
+        serie2.set("Nov", 0);
+        serie2.set("Dez", 2);
+ 
+        lineModel.addSeries(serie1);
+        lineModel.addSeries(serie2);
+        
+        Axis yAxis = lineModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Qtde");
+        yAxis.setMin(0);
+        yAxis.setMax(10);
+        yAxis.setTickCount(3);
+    }
+    
+    
     
     
     public HorizontalBarChartModel getHorizontalBarModel() {
@@ -145,6 +207,12 @@ public class RelatoriosController {
 	}
 	public void setCursoSelecionado(String cursoSelecionado) {
 		this.cursoSelecionado = cursoSelecionado;
+	}
+	public LineChartModel getLineModel() {
+		return lineModel;
+	}
+	public void setLineModel(LineChartModel lineModel) {
+		this.lineModel = lineModel;
 	}
 
 }
